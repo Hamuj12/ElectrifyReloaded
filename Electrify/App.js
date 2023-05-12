1a6e39dc1ea3bf23a2f1de385a9f835de6928a84
Hamuj12 <69257879+Hamuj12@users.noreply.github.com>
2023-05-12 00:25:30 -0500

    added cropped images display

--- Electrify/App.js
+++ Electrify/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Image, ActivityIndicator } from "react-native";
+import { StyleSheet, View, Image, ActivityIndicator, FlatList, SafeAreaView, ScrollView } from "react-native";
 import { Button } from "react-native-elements";
 import * as ImagePicker from "expo-image-picker";
 import axios from "axios";
@@ -7,6 +7,7 @@ import axios from "axios";
 export default function App() {
   const [image, setImage] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [croppedImages, setCroppedImages] = useState([]);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -18,6 +19,7 @@ export default function App() {
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
+      setCroppedImages([]);
     }
   };
 
@@ -28,6 +30,9 @@ export default function App() {
       .post("http://192.168.68.62:5000/predict", { image: base64 })
       .then((response) => {
         const base64Result = response.data.image;
+        const croppedImages = response.data.cropped_images;
+        console.log('Number of cropped images:', croppedImages.length);  // New console log
+        setCroppedImages(croppedImages);
         setImage(`data:image/jpeg;base64,${base64Result}`);
       })
       .catch((error) => {
@@ -50,17 +55,33 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Button title="Pick an image" onPress={pickImage} />
-      <Button title="Process image" onPress={processImage} disabled={!image} />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button title="Pick an image" onPress={pickImage} />
+        <Button title="Process image" onPress={processImage} disabled={!image} />
+      </View>
       {loading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        image && (
-          <Image source={{ uri: image }} style={{ width: "100%", height: "80%" }} resizeMode="contain" />
-        )
+        <View style={styles.imagesContainer}>
+          <Image source={{ uri: image }} style={styles.image} resizeMode="contain" />
+          <View style={styles.listContainer}>
+            <FlatList
+              data={croppedImages}
+              renderItem={({ item }) => (
+                <Image
+                  source={{ uri: `data:image/jpeg;base64,${item}` }}
+                  style={styles.croppedImage}
+                  resizeMode="contain"
+                />
+              )}
+              keyExtractor={(item, index) => index.toString()}
+            />
+
+          </View>
+        </View>
       )}
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -68,8 +89,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  imagesContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '40%',
+  },
+  listContainer: {
+    flex: 1,
+    width: '100%',
+  },
+  croppedImage: {
+    width: '100%',
+    height: 200,  // or any other height you prefer
   },
 });
-
--- app.py
+++ app.py
@@ -30,19 +30,38 @@ def predict():
         confidences = results[0].boxes.conf.tolist()
         classes = results[0].boxes.cls.tolist()
 
-        # Draw the bounding boxes on the image
+        cropped_images = []
+        for bbox in bboxes:
+            # Crop the image with preserved aspect ratio
+            xmin, ymin, xmax, ymax = bbox
+            width, height = xmax - xmin, ymax - ymin
+            aspect_ratio = width / height
+            
+            cropped = image.crop(bbox)
+            cropped.thumbnail((640, 640/aspect_ratio))  # Adjust size as needed, preserving aspect ratio
+
+            # Rotate the image if needed
+            if cropped.height > cropped.width:
+                cropped = cropped.rotate(90, expand=True)
+
+            buffered = io.BytesIO()
+            cropped.save(buffered, format="JPEG")
+            cropped_image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            cropped_images.append(cropped_image_base64)
+
+        # Draw the bounding boxes and confidence scores on the image
         draw = ImageDraw.Draw(image)
         for bbox, conf in zip(bboxes, confidences):
             draw.rectangle(bbox, outline="red", width=2)
             draw.text((bbox[0], bbox[1] - 10), f"{conf:.2f}", fill="red")
 
-        # Convert the image back to base64
-        buffered = io.BytesIO()
-        image.save(buffered, format="JPEG")
-        base64_image = base64.b64encode(buffered.getvalue()).decode('utf-8')
+        # Convert the final image to base64
+        final_image_buffered = io.BytesIO()
+        image.save(final_image_buffered, format="JPEG")
+        final_image_base64 = base64.b64encode(final_image_buffered.getvalue()).decode('utf-8')
 
-        # Return the base64 image
-        return jsonify({"image": base64_image})
+        # Return the base64 images
+        return jsonify({"image": final_image_base64, "cropped_images": cropped_images})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=5000)
